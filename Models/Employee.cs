aa7074103f126e3f5314fe376200a1dbd8c3c2ff
gohelhiten <gohelhiten1432@gmail.com>
2025-02-17 12:05:02 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCAPP.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateOnly HireDate { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }   
+    }
+}
